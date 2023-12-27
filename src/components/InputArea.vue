888a8a5003405a5f4160cc03621baffbda5beef3
Paul Susmarski <psusmars@nrel.gov>
2023-12-27 10:19:55 -0800

    Initial commit

---
+++ src/components/InputArea.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-textarea>
+    <!-- Your input area content goes here -->
+  </a-textarea>
+</template>
+<script>
+
+export default {
+  // Component logic goes here
+}
+</script>
+
+<style scoped>
+/* Component styles go here */
+</style>
