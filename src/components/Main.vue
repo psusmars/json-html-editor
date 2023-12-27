7becbae3038fd15886546ad14afdc1c59d1330d5
Paul Susmarski <psusmars@nrel.gov>
2023-12-27 15:05:38 -0800

    Add a button that can do something on click

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,9 +6,10 @@
 
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
         <h1 class="text-h2 font-weight-bold">J2H2J Editor</h1>
-    </v-container>
+      </v-container>
       <div class="text-body-1 mb-4">To use this application</div>
-      <v-textarea :rules="[isValidJSON]" @update:model-value="loadData" v-model="inputData" label="Paste your JSON here..."></v-textarea>
+      <v-textarea :rules="[isValidJSON]" @update:model-value="loadData" v-model="inputData"
+        label="Paste your JSON here..."></v-textarea>
       <v-btn :disabled="!canLoad" @click="loadData">
         Load
       </v-btn>
@@ -16,7 +17,7 @@
 
       <div class="py-14" />
 
-      
+
     </v-responsive>
   </v-container>
 </template>
@@ -32,8 +33,8 @@ const isValidJSON = (v: string) => {
     JSON.parse(v);
     return true;
   } catch (e) {
-    return false
+    return "Invalid JSON"
   }
 }
-const loadData = (inputValue: string) => canLoad.value = isValidJSON(inputValue)
+const loadData = (inputValue: string) => canLoad.value = isValidJSON(inputValue) === true
 </script>
