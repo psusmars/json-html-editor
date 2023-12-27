ce3678b17de6b34ba7ba35b2158ca161e0be5697
Paul Susmarski <psusmars@nrel.gov>
2023-12-27 13:58:29 -0800

    Mess around with model view

--- src/components/InputArea.vue
+++ src/components/InputArea.vue
@@ -1,9 +1,19 @@
 <template>
-  <v-textarea v-model:model-value="value" label="Paste your JSON here..."></v-textarea>
+  <v-textarea :model-value="modelValue" @update:model-value="$emit('update:modelValue', $event)"
+    :label="label"></v-textarea>
 </template>
 <script setup lang="ts">
-import {ref} from 'vue'
-const value  = ref('')
+defineEmits(['update:modelValue'])
+defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    default: 'Label'
+  }
+})
 
 </script>
 
