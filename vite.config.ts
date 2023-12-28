1ebe93a2e24f45ceb7e7995374b693e5bda16fa4
Paul Susmarski <psusmars@nrel.gov>
2023-12-27 17:25:30 -0800

    Fix the base to match the github pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'json-html-editor',
   plugins: [
     vue({
       template: { transformAssetUrls },
